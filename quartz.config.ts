641450dc3c102c032ab28aba70cf024c157938ed
liukaikai <761806790@qq.com>
2024-07-01 23:10:18 +0800

    Quartz sync: Jul 1, 2024, 11:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.kaikai.top",
+    baseUrl: "liu-kaikai.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
